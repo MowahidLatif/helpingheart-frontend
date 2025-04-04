02283473d77a6990c5ab41792fcf17cecf3fa06d
mowahid latif <mowahidlatif@hotmail.com>
2025-04-04 08:54:10 -0500

    Temp route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import HomePage from "./pages/HomePage";
 import LandingPage from "./pages/LandingPage";
+import NavBar from "./ui/NavBar";
 
 function App() {
   return (
-    <>
-      <SignUp />
-      <HomePage />
-      <LandingPage />
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/ui/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar: React.FC = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        background: "#ccc",
+        borderRadius: "5px",
+        padding: "1rem",
+        display: "flex",
+        gap: "1rem",
+      }}
+    >
+      <Link to="/">Home</Link>
+      <Link to="/home">How It Works</Link>
+      <Link to="/pricing">Pricing</Link>
+      <Link to="/about">About Us</Link>
+      <Link to="/faq">FAQ</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/signup">Sign Up</Link>
+    </div>
+  );
+};
+
+export default NavBar;
