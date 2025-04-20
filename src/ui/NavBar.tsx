c4c2a1f87c53d18e90c2e7c76692305088a7ae7f
mowahid latif <mowahidlatif@hotmail.com>
2025-04-20 11:44:58 -0500

    remove error scss

--- src/ui/NavBar.tsx
+++ src/ui/NavBar.tsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const NavBar: React.FC = () => {
   return (
     <div
-      style={{
-        width: "100%",
-        background: "#ccc",
-        borderRadius: "5px",
-        padding: "1rem",
-        display: "flex",
-        gap: "1rem",
-      }}
+      // style={{
+      //   width: "100%",
+      //   background: "#ccc",
+      //   borderRadius: "5px",
+      //   padding: "1rem",
+      //   display: "flex",
+      //   gap: "1rem",
+      // }}
     >
       <Link to="/">Home</Link>
       <Link to="/how-it-works">How It Works</Link>
